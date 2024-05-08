508aed374c23a55c4a64f9a11ec95ad2aeea0b7a
Viet Hoang <vhoang@zoodata.com.au>
2024-05-09 00:06:57 +0800

    Test

--- privatext/privatext.Client/Program.cs
+++ privatext/privatext.Client/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-builder.Services.AddHttpClient("ServerAPI", client => client.BaseAddress = new Uri($"https://localhost:{builder.Configuration["PortNumber"]}"));
+builder.Services.AddHttpClient("ServerAPI", client => client.BaseAddress = new Uri($"http://localhost:{builder.Configuration["PortNumber"]}"));
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
   .CreateClient("ServerAPI"));
 
