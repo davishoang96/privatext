932cb840a334306823f2660ce9d59b17f94a691b
Viet Hoang <vhoang@zoodata.com.au>
2024-05-11 22:06:58 +0800

    support fastendpoint

--- privatext/privatext.Client/Program.cs
+++ privatext/privatext.Client/Program.cs
@@ -1,9 +1,10 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using privatext.Client.HttpClient;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-builder.Services.AddHttpClient("ServerAPI", client => client.BaseAddress = new Uri(builder.Configuration["BaseUrl"]));
-builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
-  .CreateClient("ServerAPI"));
+builder.Services.AddScoped<IApiClient>(sp =>
+        new ApiClient(builder.Configuration["BaseUrl"],
+        new HttpClient { BaseAddress = new Uri(builder.Configuration["BaseUrl"]) }));
 
 await builder.Build().RunAsync();
