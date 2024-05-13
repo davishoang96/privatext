1ec5cc7fb5707962c6fa8bd7f91517ff99df5a34
Viet Hoang <vhoang@zoodata.com.au>
2024-05-14 00:40:14 +0800

    init

---
+++ privatext.Common/DTO/MessageDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace privatext.Common.DTO;
+
+public class MessageDTO
+{
+    public int Id { get; set; }
+    public required string Content { get; set; }
+    public DateTime DateCreated { get; set; }
+}
---
+++ privatext.Services/IMessageService.cs
@@ -0,0 +1,10 @@
+﻿using privatext.Common.DTO;
+
+namespace privatext.Services;
+
+public interface IMessageService
+{
+    MessageDTO GetMessage(int messageId);
+    Task<bool> DeleteMessage(int messageId);
+    Task<bool> CreateMessage(MessageDTO messageDTO);
+}
