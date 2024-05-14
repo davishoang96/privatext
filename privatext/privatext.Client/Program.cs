b0806afffda2393b0d12b0e5077107267f6fcdcb
Viet Hoang <vhoang@zoodata.com.au>
2024-05-15 00:56:52 +0800

    init

--- privatext/privatext.Client/Program.cs
+++ privatext/privatext.Client/Program.cs
@@ -1,10 +1,11 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using privatext.Client.HttpClient;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddScoped<IApiClient>(sp =>
         new ApiClient(builder.Configuration["BaseUrl"],
         new HttpClient { BaseAddress = new Uri(builder.Configuration["BaseUrl"]) }));
-
+builder.Services.AddRadzenComponents();
 await builder.Build().RunAsync();
