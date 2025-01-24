fc6e6573c6041d1aa67747d57b9e1cf6ff33cd2c
Davis Hoang <davishoang96@gmail.com>
2025-01-25 00:58:21 +0800

    add Quartz to delete message after certain time

---
+++ privatext.Services/DeleteMessageJob.cs
@@ -0,0 +1,24 @@
+﻿using Quartz;
+
+namespace privatext.Services;
+
+public class DeleteMessageJob : IJob
+{
+    private readonly IMessageService _messageService;
+    public DeleteMessageJob(IMessageService messageService)
+    {
+        _messageService = messageService;
+    }
+
+    public async Task Execute(IJobExecutionContext context)
+    {
+        // Get the MessageId from the job's data map
+        var messageId = context.JobDetail.JobDataMap.GetString("MessageId");
+
+        if (!string.IsNullOrEmpty(messageId))
+        {
+            // Call the message service to delete the message
+            await _messageService.DeleteMessage(messageId);
+        }
+    }
+}
